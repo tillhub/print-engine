5b6fe38cd7b8fd9cc76a0c8913700b160a50bde1
Martin <m.sirok@gmail.com>
2025-02-18 12:48:12 +0100

    simplified HTML printing implementation

--- print-engine/src/main/java/de/tillhub/printengine/verifone/VerifonePrintController.kt
+++ print-engine/src/main/java/de/tillhub/printengine/verifone/VerifonePrintController.kt
@@ -3,36 +3,34 @@ package de.tillhub.printengine.verifone
 import android.graphics.Bitmap
 import com.verifone.peripherals.DirectPrintManager
 import com.verifone.peripherals.Printer
-import de.tillhub.printengine.PrinterController
+import de.tillhub.printengine.HtmlUtils.FEED_PAPER
+import de.tillhub.printengine.HtmlUtils.generateImageHtml
+import de.tillhub.printengine.HtmlUtils.transformToHtml
+import de.tillhub.printengine.barcode.BarcodeEncoder
 import de.tillhub.printengine.data.PrinterInfo
 import de.tillhub.printengine.data.PrinterServiceVersion
 import de.tillhub.printengine.data.PrinterState
 import de.tillhub.printengine.data.PrintingFontType
 import de.tillhub.printengine.data.PrintingIntensity
 import de.tillhub.printengine.data.PrintingPaperSpec
-import de.tillhub.printengine.data.RawPrinterData
-import de.tillhub.printengine.barcode.BarcodeEncoder
-import de.tillhub.printengine.barcode.BarcodeType
-import de.tillhub.printengine.HtmlUtils.FEED_PAPER
-import de.tillhub.printengine.HtmlUtils.generateImageHtml
-import de.tillhub.printengine.HtmlUtils.monospaceText
-import de.tillhub.printengine.HtmlUtils.singleLineCenteredText
-import de.tillhub.printengine.HtmlUtils.transformToHtml
+import de.tillhub.printengine.html.HtmlPrinterController
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 internal class VerifonePrintController(
     private val printManager: DirectPrintManager,
     private val printerState: MutableStateFlow<PrinterState>,
-    private val barcodeEncoder: BarcodeEncoder,
-    /**
-     * If this field is set to false each print command is handled separately.
-     * If it is set to true the print commands are grouped and handled when start() is called
-     */
-    private val batchPrint: Boolean = BATCH_PRINT_DEFAULT
-) : PrinterController {
-
-    private val batchSB = StringBuilder()
+    barcodeEncoder: BarcodeEncoder,
+) : HtmlPrinterController(
+    printerState = printerState,
+    barcodeEncoder = barcodeEncoder,
+    batchPrint = BATCH_PRINT_DEFAULT,
+    barcodeWidth = BARCODE_WIDTH,
+    barcodeHeight = BARCODE_HEIGHT,
+    qrCodeSize = QR_CODE_SIZE,
+    fontSize = FONT_SIZE,
+    includeStyleTag = false,
+    feedString = FEED_PAPER
+) {
     private var useCutter = false
 
     private val printListener: DirectPrintManager.DirectPrintListener by lazy {
@@ -64,53 +62,26 @@ internal class VerifonePrintController(
         }
     }
 
-    override fun sendRawData(data: RawPrinterData) {
-        printText(data.bytes.toString(Charsets.UTF_8))
-    }
+    override fun printContent(content: String, cutAfterPrint: Boolean) {
+        printManager.printString(
+            printListener,
+            transformToHtml(batchSB.toString()),
+            when {
+                cutAfterPrint || useCutter -> Printer.PRINTER_FULL_CUT
+                else -> Printer.PRINTER_NO_CUTTER_LINE_FEED
+            }
+        )
 
-    override fun observePrinterState(): StateFlow<PrinterState> = printerState
+        useCutter = false
+    }
 
     override fun setFontSize(fontSize: PrintingFontType) = Unit // Not supported
-
-    override fun printText(text: String) {
-        if (batchPrint) {
-            batchSB.appendLine(monospaceText(text))
-        } else {
-            printManager.printString(
-                printListener,
-                transformToHtml(monospaceText(text)),
-                Printer.PRINTER_NO_CUTTER_LINE_FEED
-            )
-        }
-    }
-
-    override fun printBarcode(barcode: String) {
-        barcodeEncoder.encodeAsBitmap(barcode, BarcodeType.CODE_128, BARCODE_WIDTH, BARCODE_HEIGHT)?.let { image ->
-            printImage(image)
-            printText(singleLineCenteredText(barcode))
-        }
-    }
-
-    override fun printQr(qrData: String) {
-        barcodeEncoder.encodeAsBitmap(qrData, BarcodeType.QR_CODE, QR_CODE_SIZE, QR_CODE_SIZE)?.let { image ->
-            printImage(image)
-            printText(singleLineCenteredText(qrData))
-        }
-    }
 
     override fun printImage(image: Bitmap) {
         if (batchPrint) {
             batchSB.append(generateImageHtml(image))
         } else {
             printManager.printBitmap(printListener, image, Printer.PRINTER_NO_CUTTER_LINE_FEED)
-        }
-    }
-
-    override fun feedPaper() {
-        if (batchPrint) {
-            batchSB.append(FEED_PAPER)
-        } else {
-            printManager.printString(printListener, "", Printer.PRINTER_NO_CUT)
         }
     }
 
@@ -123,19 +94,6 @@ internal class VerifonePrintController(
     }
 
     override fun setIntensity(intensity: PrintingIntensity) = Unit // Not supported
-
-    override fun start() {
-        if (batchPrint && batchSB.isNotEmpty()) {
-            printManager.printString(
-                printListener,
-                transformToHtml(batchSB.toString()),
-                if (useCutter) Printer.PRINTER_FULL_CUT else Printer.PRINTER_NO_CUTTER_LINE_FEED
-            )
-
-            batchSB.clear()
-            useCutter = false
-        }
-    }
 
     override suspend fun getPrinterInfo(): PrinterInfo =
         PrinterInfo(
@@ -154,5 +112,6 @@ internal class VerifonePrintController(
         private const val BARCODE_HEIGHT = 140
         private const val BARCODE_WIDTH = 420
         private const val QR_CODE_SIZE = 420
+        private const val FONT_SIZE = 20
     }
 }
