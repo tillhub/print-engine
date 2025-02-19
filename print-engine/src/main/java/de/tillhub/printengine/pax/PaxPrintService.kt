72f54e85dd86808c9d9a9b6d1ff1fc861651ba89
Đorđe Hrnjez <djordjehrnjez@gmail.com>
2025-02-19 14:46:07 +0100

    pax improvements (in progress)

--- print-engine/src/main/java/de/tillhub/printengine/pax/PaxPrintService.kt
+++ print-engine/src/main/java/de/tillhub/printengine/pax/PaxPrintService.kt
@@ -1,40 +1,56 @@
 package de.tillhub.printengine.pax
 
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import android.os.Messenger
 import de.tillhub.printengine.PrintService
 import de.tillhub.printengine.PrinterController
 import de.tillhub.printengine.barcode.BarcodeEncoderImpl
 import de.tillhub.printengine.data.PrinterState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import timber.log.Timber
 
 /**
  * Print service for encapsulating connection handling, error handling and convenience methods for working with
  * [PaxPrinterController].
  */
 internal class PaxPrintService(context: Context) : PrintService() {
+
     override var printController: PrinterController? = null
 
     private val connectionState = MutableStateFlow<PrinterState>(PrinterState.CheckingForPrinter)
     override val printerState: StateFlow<PrinterState> = connectionState
 
-    init {
-        @Suppress("TooGenericExceptionCaught")
-        try {
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder) {
             printController = PaxPrinterController(
                 printerState = connectionState,
-                paxPrinterConnector = PaxPrinterConnector(
-                    context = context,
-                    printerState = connectionState
-                ),
+                messenger = Messenger(service),
                 barcodeEncoder = BarcodeEncoderImpl()
-
             )
-            connectionState.value = PrinterState.Connected
-        } catch (e: Exception) {
-            Timber.e(e)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
             connectionState.value = PrinterState.Error.NotAvailable
         }
+    }
+
+    init {
+        val intent = Intent().apply {
+            component = ComponentName(PRINTING_PACKAGE, PRINTING_CLASS)
+        }
+        if (!context.bindService(intent, connection, Context.BIND_AUTO_CREATE)) {
+            context.unbindService(connection)
+            connectionState.value = PrinterState.Error.NotAvailable
+        }
+    }
+
+    companion object {
+        private const val PRINTING_PACKAGE = "de.ccv.payment.printservice"
+        private const val PRINTING_CLASS = "de.ccv.payment.printservice.DirectPrintService"
     }
 }
