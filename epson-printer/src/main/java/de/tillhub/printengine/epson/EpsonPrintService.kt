596e96b42717efe1c99add9018cce822a2d0812b
Martin <m.sirok@gmail.com>
2025-04-10 10:24:36 +0200

    [UNTIL-13460] addressed issues found during testing: - model missmatch

--- epson-printer/src/main/java/de/tillhub/printengine/epson/EpsonPrintService.kt
+++ epson-printer/src/main/java/de/tillhub/printengine/epson/EpsonPrintService.kt
@@ -82,44 +82,44 @@ class EpsonPrintService(context: Context, printer: ExternalPrinter) : PrintServi
 
     @Suppress("CyclomaticComplexMethod")
     private fun String.toModel() = when (this) {
-        "TM_M10" -> EpsonPrinter.TM_M10
-        "TM_M30" -> EpsonPrinter.TM_M30
-        "TM_P20" -> EpsonPrinter.TM_P20
-        "TM_P60" -> EpsonPrinter.TM_P60
-        "TM_P60II" -> EpsonPrinter.TM_P60II
-        "TM_P80" -> EpsonPrinter.TM_P80
-        "TM_T20" -> EpsonPrinter.TM_T20
-        "TM_T60" -> EpsonPrinter.TM_T60
-        "TM_T70" -> EpsonPrinter.TM_T70
-        "TM_T81" -> EpsonPrinter.TM_T81
-        "TM_T82" -> EpsonPrinter.TM_T82
-        "TM_T83" -> EpsonPrinter.TM_T83
-        "TM_T88" -> EpsonPrinter.TM_T88
-        "TM_T90" -> EpsonPrinter.TM_T90
-        "TM_T90KP" -> EpsonPrinter.TM_T90KP
-        "TM_U220" -> EpsonPrinter.TM_U220
-        "TM_U330" -> EpsonPrinter.TM_U330
-        "TM_L90" -> EpsonPrinter.TM_L90
-        "TM_H6000" -> EpsonPrinter.TM_H6000
-        "TM_T83III" -> EpsonPrinter.TM_T83III
-        "TM_T100" -> EpsonPrinter.TM_T100
-        "TM_M30II" -> EpsonPrinter.TM_M30II
-        "TS_100" -> EpsonPrinter.TS_100
-        "TM_M50" -> EpsonPrinter.TM_M50
-        "TM_T88VII" -> EpsonPrinter.TM_T88VII
-        "TM_L90LFC" -> EpsonPrinter.TM_L90LFC
-        "EU_M30" -> EpsonPrinter.EU_M30
-        "TM_L100" -> EpsonPrinter.TM_L100
-        "TM_P20II" -> EpsonPrinter.TM_P20II
-        "TM_P80II" -> EpsonPrinter.TM_P80II
-        "TM_M30III" -> EpsonPrinter.TM_M30III
-        "TM_M50II" -> EpsonPrinter.TM_M50II
-        "TM_M55" -> EpsonPrinter.TM_M55
-        "TM_U220II" -> EpsonPrinter.TM_U220II
-        "SB_H50" -> EpsonPrinter.SB_H50
+        "TM-M10" -> EpsonPrinter.TM_M10
+        "TM-M30" -> EpsonPrinter.TM_M30
+        "TM-P20" -> EpsonPrinter.TM_P20
+        "TM-P60" -> EpsonPrinter.TM_P60
+        "TM-P60II" -> EpsonPrinter.TM_P60II
+        "TM-P80" -> EpsonPrinter.TM_P80
+        "TM-T20" -> EpsonPrinter.TM_T20
+        "TM-T60" -> EpsonPrinter.TM_T60
+        "TM-T70" -> EpsonPrinter.TM_T70
+        "TM-T81" -> EpsonPrinter.TM_T81
+        "TM-T82" -> EpsonPrinter.TM_T82
+        "TM-T83" -> EpsonPrinter.TM_T83
+        "TM-T88" -> EpsonPrinter.TM_T88
+        "TM-T90" -> EpsonPrinter.TM_T90
+        "TM-T90KP" -> EpsonPrinter.TM_T90KP
+        "TM-U220" -> EpsonPrinter.TM_U220
+        "TM-U330" -> EpsonPrinter.TM_U330
+        "TM-L90" -> EpsonPrinter.TM_L90
+        "TM-H6000" -> EpsonPrinter.TM_H6000
+        "TM-T83III" -> EpsonPrinter.TM_T83III
+        "TM-T100" -> EpsonPrinter.TM_T100
+        "TM-M30II" -> EpsonPrinter.TM_M30II
+        "TS-100" -> EpsonPrinter.TS_100
+        "TM-M50" -> EpsonPrinter.TM_M50
+        "TM-T88VII" -> EpsonPrinter.TM_T88VII
+        "TM-L90LFC" -> EpsonPrinter.TM_L90LFC
+        "EU-M30" -> EpsonPrinter.EU_M30
+        "TM-L100" -> EpsonPrinter.TM_L100
+        "TM-P20II" -> EpsonPrinter.TM_P20II
+        "TM-P80II" -> EpsonPrinter.TM_P80II
+        "TM-M30III" -> EpsonPrinter.TM_M30III
+        "TM-M50II" -> EpsonPrinter.TM_M50II
+        "TM-M55" -> EpsonPrinter.TM_M55
+        "TM-U220II" -> EpsonPrinter.TM_U220II
+        "SB-H50" -> EpsonPrinter.SB_H50
 
         // based on the documentation these 2 can be valid
-        "TM Printer" -> EpsonPrinter.TM_T88
+        "TM PRINTER",
         "" -> EpsonPrinter.TM_T88
         else -> throw IllegalArgumentException("Unsupported printer type: $this")
     }
