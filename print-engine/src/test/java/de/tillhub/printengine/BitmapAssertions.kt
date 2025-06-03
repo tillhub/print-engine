bd8ed1b6751b3bd84e79495b817a3c2b10429cf9
Martin <m.sirok@gmail.com>
2025-06-03 09:27:36 +0200

    [HOTFIX] added barcode tests that compare generated bitmaps

---
+++ print-engine/src/test/java/de/tillhub/printengine/BitmapAssertions.kt
@@ -0,0 +1,38 @@
+package de.tillhub.printengine
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.test.platform.app.InstrumentationRegistry
+
+object BitmapAssertions {
+    fun assertBitmapMatches(
+        goldenName: String,
+        bitmap: Bitmap,
+    ) {
+        val golden = InstrumentationRegistry.getInstrumentation()
+            .context.resources.assets.open("$goldenName.png").use { BitmapFactory.decodeStream(it) }
+
+        golden.compare(bitmap)
+    }
+
+    private fun Bitmap.compare(other: Bitmap) {
+        if (this.width != other.width || this.height != other.height) {
+            throw AssertionError("Size of screenshot does not match asset file (check device density)")
+        }
+        // Compare row by row to save memory on device
+        val row1 = IntArray(width)
+        val row2 = IntArray(width)
+        for (column in 0 until height) {
+            // Read one row per bitmap and compare
+            this.getRow(row1, column)
+            other.getRow(row2, column)
+            if (!row1.contentEquals(row2)) {
+                throw AssertionError("Sizes match but bitmap content has differences")
+            }
+        }
+    }
+
+    private fun Bitmap.getRow(pixels: IntArray, column: Int) {
+        this.getPixels(pixels, 0, width, 0, column, width, 1)
+    }
+}
