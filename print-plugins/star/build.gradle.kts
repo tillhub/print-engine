0de84b1db8b05fe7594ddebf0fe225e6b0138f46
shekar-allam <89923464+shekar-allam@users.noreply.github.com>
2025-10-28 11:11:01 +0100

    [UNTIL-15756]Update star sdk v1.11.0

--- print-plugins/star/build.gradle.kts
+++ print-plugins/star/build.gradle.kts
@@ -39,6 +39,8 @@ android {
         useJUnitPlatform()
     }
 
+    packaging.jniLibs.useLegacyPackaging = true
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             jvmTarget = Configs.JVM_TARGET
